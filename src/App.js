854070a215a874f0a4c2bbb10ae2189e6bf93907
yuma-ueki <yuma-ueki@kccs.co.jp>
2023-08-31 14:56:33 +0900

    t

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Amplify, API, graphqlOperation } from 'aws-amplify';
+import {Amplify,  API, graphqlOperation } from 'aws-amplify';
 import { listTodos } from './graphql/queries';
 import { createTodo, deleteTodo } from './graphql/mutations';
 import config from './aws-exports';
@@ -10,6 +10,7 @@ Amplify.configure(config);
 function App() {
   const [todos, setTodos] = useState([]);
   const [formData, setFormData] = useState({ name: '', description: '' });
+  const [selectedTodos, setSelectedTodos] = useState([]);
 
   useEffect(() => {
     fetchTodos();
@@ -26,10 +27,10 @@ function App() {
 
   const addTodo = async () => {
     try {
-      if (!formData.name || !formData.description) return;
+      if (!formData.name) return;
       await API.graphql(graphqlOperation(createTodo, { input: formData }));
-      fetchTodos();  // Refresh the list after adding
-      setFormData({ name: '', description: '' });  // Clear the form
+      setTodos([...todos, formData]);
+      setFormData({ name: '', description: '' });
     } catch (err) {
       console.error('Error adding todo:', err);
     }
@@ -38,22 +39,24 @@ function App() {
   const removeTodo = async (id) => {
     try {
       await API.graphql(graphqlOperation(deleteTodo, { input: { id } }));
-      fetchTodos();  // Refresh the list after deleting
+      const newTodos = todos.filter(todo => todo.id !== id);
+      setTodos(newTodos);
     } catch (err) {
       console.error('Error deleting todo:', err);
     }
   }
 
-  const removeAllTodos = async () => {
-    try {
-      for (const todo of todos) {
-        await API.graphql(graphqlOperation(deleteTodo, { input: { id: todo.id } }));
-      }
-      setTodos([]);  // Clear the list in the UI
-    } catch (err) {
-      console.error('Error deleting all todos:', err);
+  const toggleSelected = id => {
+    const isAlreadySelected = selectedTodos.includes(id);
+    setSelectedTodos(prev => isAlreadySelected ? prev.filter(todoId => todoId !== id) : [...prev, id]);
+  };
+
+  const deleteSelectedTodos = async () => {
+    for (const id of selectedTodos) {
+      await removeTodo(id);
     }
-  }
+    setSelectedTodos([]);  // Clear selections
+  };
 
   return (
     <div className="App">
@@ -69,10 +72,12 @@ function App() {
         placeholder="Todo description"
       />
       <button onClick={addTodo}>Add Todo</button>
-      <button onClick={removeAllTodos}>Delete All Todos</button>
+      <button onClick={fetchTodos}>Fetch Todos</button>
+
       <table>
         <thead>
           <tr>
+            <th>Select</th>
             <th>Name</th>
             <th>Description</th>
             <th>Action</th>
@@ -81,7 +86,14 @@ function App() {
         <tbody>
           {
             todos.map(todo => (
-              <tr key={todo.id}>
+              <tr key={todo.id || todo.name}>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={selectedTodos.includes(todo.id)}
+                    onChange={() => toggleSelected(todo.id)}
+                  />
+                </td>
                 <td>{todo.name}</td>
                 <td>{todo.description}</td>
                 <td><button onClick={() => removeTodo(todo.id)}>Delete</button></td>
@@ -90,6 +102,7 @@ function App() {
           }
         </tbody>
       </table>
+      <button onClick={deleteSelectedTodos}>Delete Selected</button>
     </div>
   );
 }
